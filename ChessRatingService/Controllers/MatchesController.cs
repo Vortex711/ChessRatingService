31a15cddaa91cc672f2e019729cd12b1bb9e76dd
Chinmay <chinmayhari711@gmail.com>
2026-01-07 22:54:53 +0530

    Add API endpoint for match processing

---
+++ ChessRatingService/Controllers/MatchesController.cs
@@ -0,0 +1,32 @@
+﻿using ChessRatingService.Services;
+using Microsoft.AspNetCore.Mvc;
+using ChessRatingService.DTOs;
+
+namespace ChessRatingService.Controllers
+{
+    [Route("api/matches")]
+    [ApiController]
+    public class MatchesController : ControllerBase
+    {
+        private readonly MatchProcessingService _matchService;
+
+        public MatchesController(MatchProcessingService matchService)
+        {
+            _matchService = matchService;
+        }
+
+        [HttpPost("process")]
+        public IActionResult ProcessMatch(ProcessMatchRequest request)
+        {
+            _matchService.ProcessMatch(
+                request.ExternalMatchId,
+                request.PlayerAId,
+                request.PlayerBId,
+                request.Result,
+                request.PlayedAt
+            );
+
+            return Ok();
+        }
+    }
+}
---
+++ ChessRatingService/DTOs/ProcessMatchRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace ChessRatingService.DTOs
+{
+    public class ProcessMatchRequest
+    {
+        public required string ExternalMatchId { get; set; }
+        public int PlayerAId { get; set; }
+        public int PlayerBId { get; set; }
+        public int Result { get; set; }
+        public DateTime PlayedAt { get; set; }
+    }
+}
